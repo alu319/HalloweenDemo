90ce6c8fd7496b6a72745ec72fcc4ebc940202a1
Samuel Tay <samueltay@berkeley.edu>
2015-10-21 13:29:04 -0700

    First group commit

---
+++ Assets/Dimming.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dimming : MonoBehaviour {
+
+    public Light lightToDim = null;
+    public float maxTime = 30; //30 seconds
+    public float intensity = 2;
+    private float mStartTime = 0;
+
+    private void Update()
+    {
+        mStartTime = mStartTime + Time.deltaTime;
+        if (lightToDim)
+        lightToDim.intensity = (intensity - mStartTime/maxTime);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            mStartTime = -Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            mStartTime = -Time.deltaTime;
+        }
+    }
+
+    }
